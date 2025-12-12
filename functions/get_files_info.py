36f7b624a9e12bb708614c183299b4faebb21d59
Eric Phillips <eric@ewpt3ch.com>
2025-12-11 21:11:54 -0700

    ch2l3

---
+++ functions/get_files_info.py
@@ -0,0 +1,27 @@
+import os
+
+
+
+def get_files_info(working_directory, directory="."):
+
+    try:
+        #get the full path and check it's in the correct working dir
+        fullpath = os.path.join(working_directory, directory)
+        fullpath = os.path.abspath(fullpath)
+        if not os.path.isdir(fullpath):
+            return f'Error: "{directory}" is not a directory'
+        if not fullpath.startswith(os.path.abspath(working_directory)):
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+        # get the contents of directory
+        dirlisting = os.listdir(fullpath)
+        filelist = ''
+        for file in dirlisting:
+            fsize = os.path.getsize(os.path.join(fullpath, file))
+            diryn = os.path.isdir(os.path.join(fullpath, file))
+            filelist += (f'- {file}: file_size={fsize} bytes, is_dir={diryn}\n')
+
+        return filelist
+
+    except Exception as errmes:
+        return f"An unexpected error occured: {errmes}"
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 model = "gemini-2.5-flash"
 
@@ -18,18 +19,23 @@ def main():
     client = genai.Client(api_key=api_key)
 
     parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     args = parser.parse_args()
     # Now we can access `args.user_prompt`
 
-    prompt = args.user_prompt
-    response = client.models.generate_content(model=model, contents=prompt)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    response = client.models.generate_content(
+            model=model, contents=messages)
 
     if response.usage_metadata == None:
         raise RuntimeError("no api response")
     prompt_tokens = response.usage_metadata.prompt_token_count
     response_tokens = response.usage_metadata.candidates_token_count
-    print(f'Prompt tokens: {prompt_tokens}\nResponse tokens: {response_tokens}')
+    if args.verbose:
+        print(f'User prompt: {args.user_prompt}')
+        print(f'Prompt tokens: {prompt_tokens}\nResponse tokens: {response_tokens}')
     print(response.text)
 
 if __name__ == "__main__":
